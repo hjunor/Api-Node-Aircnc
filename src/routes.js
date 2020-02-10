9e99bee64ce925b46dd45c117b408633cf21ff13
Heberth Junor Teixeira Aguiar <hjunor@gmail.com>
2020-02-09 21:25:44 -0300

    Rotas aplicação

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,17 @@
 const express = require('express');
+const multer = require('multer');
+const UpConfig = require('./config/upload');
 
+const SessionControllers = require('./controllers/SessionControllers');
+const SpotControllers = require('./controllers/SpotControllers');
+const DashControllers = require('./controllers/DashboardControllers');
+const BookControllers = require('./controllers/BookingControllers')
 const routes = express.Router();
+const upload = multer(UpConfig);
 
-routes.post('/users', function (req, res) {
-  return res.json(req.body);
-})
-
+routes.post('/sessions',SessionControllers.store)
+routes.post('/spot',upload.single('thumbnail'),SpotControllers.store)
+routes.get('/spot',SpotControllers.index)
+routes.get('/dash',DashControllers.show)
+routes.post('/spot/:spot_id/book', BookControllers.store)
 module.exports = routes;
